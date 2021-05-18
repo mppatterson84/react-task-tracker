3894c42fea960656edd979bf13156b005c5f9b95
mppatterson84 <mppatterson84@gmail.com>
2021-05-18 11:45:28 -0500

    add useState to App to use globally

--- src/App.js
+++ src/App.js
@@ -1,11 +1,32 @@
+import { useState } from 'react';
 import Header from './components/Header';
 import Tasks from './components/Tasks';
 
 function App() {
+  const [tasks, setTasks] = useState([
+    {
+      id: 1,
+      text: 'Task 1',
+      day: 'Feb 5th at 2:30pm',
+      reminder: true
+    },
+    {
+      id: 2,
+      text: 'Task 2',
+      day: 'Feb 6th at 1:30pm',
+      reminder: true
+    },
+    {
+      id: 3,
+      text: 'Task 3',
+      day: 'Feb 7th at 12:30pm',
+      reminder: false
+    }
+  ]);
   return (
     <div className="container">
       <Header />
-      <Tasks />
+      <Tasks tasks={tasks} />
     </div>
   );
 }
